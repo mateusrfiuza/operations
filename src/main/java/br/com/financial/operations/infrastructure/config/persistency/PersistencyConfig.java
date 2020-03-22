0703be2ada5e613425c66dd56e7cbe1f5dcc34a6
Mateus Fiuza <ive_mfiuza@uolinc.com>
2020-03-22 17:15:05 -0300

    config de persistency spring data

---
+++ src/main/java/br/com/financial/operations/infrastructure/config/persistency/PersistencyConfig.java
@@ -0,0 +1,12 @@
+package br.com.financial.operations.infrastructure.config.persistency;
+
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaRepositories("br.com.financial.operations.infrastructure.database")
+@EntityScan("br.com.financial.operations.infrastructure.database")
+public class PersistencyConfig {
+
+}
