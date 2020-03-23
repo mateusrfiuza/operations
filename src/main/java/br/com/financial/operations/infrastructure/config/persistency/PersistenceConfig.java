2c97c76d7b23147c8519602f88a553805fb94b82
Mateus Fiuza <ive_mfiuza@uolinc.com>
2020-03-23 17:49:57 -0300

    Ajuste nome da classe de persistência

--- src/main/java/br/com/financial/operations/infrastructure/config/persistency/PersistenceConfig.java
+++ src/main/java/br/com/financial/operations/infrastructure/config/persistency/PersistenceConfig.java
@@ -7,6 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @EnableJpaRepositories("br.com.financial.operations.infrastructure.database")
 @EntityScan("br.com.financial.operations.infrastructure.database")
-public class PersistencyConfig {
+public class PersistenceConfig {
 
 }
